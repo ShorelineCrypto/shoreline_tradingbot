414db09c91e728298596d15c8f16ed84ea8722fc
ShorelineCrypto <admin@shorelinecrypto.com>
2019-09-08 22:09:26 -0400

    first api try

--- tradingbot/tradingbot.py
+++ tradingbot/tradingbot.py
@@ -4,6 +4,26 @@ import hashlib
 import requests
 
 def place_trade(config):
+   apifunc = '/account/getdepositaddress'
+   currency = config['currency']
+   apikey = config['apikey']
+   apisecret = config['apisecret']
    
+   nonce =  str(int(time.time()))
+   uri = 'https://shorelinecrypto.com/api/v1' + apifunc + '?apikey=' +  apikey + '&nonce=' + nonce + '&currency=' +  currency
+   path =   '/api/v1' + apifunc + '?apikey=' +  apikey + '&nonce=' + nonce + '&currency=' +  currency
    
+   signature = hmac.new(apisecret, msg=uri, digestmod=hashlib.sha512).hexdigest()
+   
+   print(uri)
+   print(signature)
+   datadict = {'host': 'shorelinecrypto.com',
+            'path': path
+            }
 
+   headerdict = {'apisign':  signature}
+
+   request = requests.get(
+      url=uri, data=datadict, headers = headerdict)
+
+   print(request.text)
