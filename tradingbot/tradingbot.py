1b91baec7b7b5475e46620c74378e828db29237a
ShorelineCrypto <admin@shorelinecrypto.com>
2019-12-17 17:55:04 -0500

    implemented feature to cancel existing order first to avoid duplicate

--- tradingbot/tradingbot.py
+++ tradingbot/tradingbot.py
@@ -2,6 +2,7 @@ import time
 import hmac
 import hashlib
 import requests
+import json
 
 def check_depositaddr(config):
    apifunc = '/account/getdepositaddress'
@@ -27,9 +28,59 @@ def check_depositaddr(config):
       url=uri, data=datadict, headers = headerdict)
 
    print(request.text)
+   
 
+def clear_allorders(config):
+   apifunc = '/market/getopenorders'
+   apikey = config['apikey']
+   apisecret = config['apisecret']
+   market = config['maincoin'] + '-' + config['currency']
+   nonce =  str(int(time.time()))
+   path =   '/api/v1' + apifunc + '?apikey=' +  apikey + '&nonce=' + nonce + '&market=' + market
+   uri = 'https://shorelinecrypto.com' + path
+   signature = hmac.new(apisecret, msg=uri, digestmod=hashlib.sha512).hexdigest()
+   
+   # print(uri)
+   # print(signature)
+   datadict = {'host': 'shorelinecrypto.com',
+            'path': path
+            }
 
+   headerdict = {'apisign':  signature}
+
+   request = requests.get(
+      url=uri, data=datadict, headers = headerdict)
+
+   
+   out_dict = json.loads(request.text)
+   # print(out_dict)
+   for myorder in out_dict['result']:
+      if float(myorder['Price']) == config['price']:
+           cancel_order(config, myorder['OrderUuid'])
+
+def cancel_order(config, uuid):
+   apifunc = '/market/cancel'
+   apikey = config['apikey']
+   apisecret = config['apisecret']
+   nonce =  str(int(time.time()))
+   path =   '/api/v1' + apifunc + '?apikey=' +  apikey + '&nonce=' + nonce + '&uuid=' + uuid
+   uri = 'https://shorelinecrypto.com' + path
+   
+   signature = hmac.new(apisecret, msg=uri, digestmod=hashlib.sha512).hexdigest()
+   datadict = {'host': 'shorelinecrypto.com',
+            'path': path
+            }
+
+   headerdict = {'apisign':  signature}
+
+   request = requests.get(
+      url=uri, data=datadict, headers = headerdict)
+
+   print(request.text)
+
+   
 def place_trade(config):
+   clear_allorders(config)
    if (config['strategy'] == 'sellonly'):
       apifunc = '/market/selllimit'
    else:
